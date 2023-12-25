adcc18a62a300f4ee0a80af5c64535ac69c3d9c7
muhammed <muhammedkalafat55@gmail.com>
2023-12-25 21:25:29 +0300

    calismalarim

--- 02_Day_Data_types/math_object.js
+++ 02_Day_Data_types/math_object.js
@@ -1,34 +1,48 @@
-const PI = Math.PI
-console.log(PI) // 3.141592653589793
-console.log(Math.round(PI)) // 3; to round values to the nearest number
-console.log(Math.round(9.81)) // 10
-console.log(Math.floor(PI)) // 3; rounding down
-console.log(Math.ceil(PI)) // 4; rounding up
-console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
-console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
+const PI = Math.PI;
+console.log(PI); // 3.141592653589793
+console.log(Math.round(PI)); // 3; to round values to the nearest number
+console.log(Math.round(9.81)); // 10
+console.log(Math.floor(PI)); // 3; rounding down
+console.log(Math.ceil(PI)); // 4; rounding up
+console.log(Math.min(-5, 3, 20, 4, 5, 10)); // -5, returns the minimum value
+console.log(Math.max(-5, 3, 20, 4, 5, 10)); // 20, returns the maximum value
 
-const randNum = Math.random() // creates random number between 0 to 0.999999
-console.log(randNum)
+const randNum = Math.random(); // creates random number between 0 to 0.999999
+console.log(randNum);
 // Let create random number between 0 to 10
-const num = Math.floor(Math.random() * 11) // creates random number between 0 and 10
-console.log(num)
+const num = Math.floor(Math.random() * 11); // creates random number between 0 and 10
+console.log(num);
 
 //Absolute value
-console.log(Math.abs(-10)) //10
+console.log(Math.abs(-10)); //10
 //Square root
-console.log(Math.sqrt(100)) // 10
-console.log(Math.sqrt(2)) //1.4142135623730951
+console.log(Math.sqrt(100)); // 10
+console.log(Math.sqrt(2)); //1.4142135623730951
 // Power
-console.log(Math.pow(3, 2)) // 9
-console.log(Math.E) // 2.718
+console.log(Math.pow(3, 2)); // 9
+console.log(Math.E); // 2.718
 
 // Logarithm
 //Returns the natural logarithm of base E of x, Math.log(x)
-console.log(Math.log(2)) // 0.6931471805599453
-console.log(Math.log(10)) // 2.302585092994046
+console.log(Math.log(2)); // 0.6931471805599453
+console.log(Math.log(10)); // 2.302585092994046
 
 // Trigonometry
-console.log(Math.sin(0))
-console.log(Math.sin(60))
-console.log(Math.cos(0))
-console.log(Math.cos(60))
+console.log(Math.sin(0));
+console.log(Math.sin(60));
+console.log(Math.cos(0));
+console.log(Math.cos(60));
+
+console.log(PI);
+console.log(Math.min(-5, 4, 15, 22));
+
+console.log(Math.floor(Math.random() * 11));
+
+console.log(Math.abs(-10));
+
+console.log(Math.sqrt(100));
+
+console.log(Math.pow(3, 2));
+
+console.log(Math.E);
+
